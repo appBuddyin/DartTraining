7beeaaf7b21f65f044d630c85a628dfd07b4b66e
POOJA <pooja@appbuddy.in>
2022-11-30 19:05:21 +0530

    controlflow program commit

---
+++ If else statement.dart
@@ -0,0 +1,12 @@
+void main() {
+  int gfg = 10;
+
+  // Condition is false
+  if (gfg > 30) {
+    // This will not be printed
+    print("Condition is true");
+  } else {
+    // This will be printed
+    print("Condition id false");
+  }
+}
---
+++ If statement.dart
@@ -0,0 +1,9 @@
+void main() {
+  int gfg = 10;
+
+  // Condition is true
+  if (gfg > 3) {
+    // This will be printed
+    print("Condition is true");
+  }
+}
