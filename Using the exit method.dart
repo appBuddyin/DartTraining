1db6eb9676fe6deca33108020af99d03dab5f1d9
POOJA <pooja@appbuddy.in>
2022-12-05 18:29:14 +0530

    key functions commit

---
+++ Using the exit method.dart
@@ -0,0 +1,14 @@
+// Importing the packages
+import 'dart:io';
+
+// Main Function
+void main() {
+  // This will be printed
+  print("Hello GeeksForGeeks");
+
+  // Standard out code
+  exit(0);
+
+  // This will not be printed
+  print("Good Bye GeeksForGeeks");
+}
